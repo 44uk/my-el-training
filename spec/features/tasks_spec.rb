e424dd32561f5389146facdb4f42faacefbeabf5
Yoshiyuki Ieyama <yukku0423@gmail.com>
2018-03-11 01:53:15 +0900

    add task feature spec

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "user create a new task" do
+    visit tasks_path
+    expect {
+      click_link '登録'
+      fill_in 'Title', with: 'A Task'
+      fill_in 'Description', with: 'A simple task'
+      click_button '登録'
+
+      expect(page).to have_content '登録完了'
+      expect(page).to have_content 'A Task'
+      expect(page).to have_content 'A simple task'
+    }.to change(Task.all, :count).by(1)
+  end
+
+  scenario "user edit a task" do
+    Task.create(title: 'A task', description: 'A simple task')
+    visit tasks_path
+
+    click_link '編集'
+    fill_in 'Title', with: 'B task'
+    fill_in 'Description', with: 'B hard task'
+    click_button '更新'
+
+    expect(page).to have_content '更新完了'
+    expect(page).to have_content 'B task'
+    expect(page).to have_content 'B hard task'
+  end
+
+  scenario "user delete a task", js: true do
+    Task.create(title: 'A task', description: 'A simple task')
+    visit tasks_path
+
+    click_link '削除'
+    page.accept_confirm
+
+    expect {
+      expect(page).to have_content '削除完了'
+      expect(page).not_to have_content 'A Task'
+      expect(page).not_to have_content 'A simple task'
+    }.to change(Task.all, :count).by(-1)
+  end
 end
